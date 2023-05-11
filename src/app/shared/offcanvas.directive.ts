297f6040d18e97e492572aa3cbb068b3c863a789
zumbesa1 <salahudin.z@hotmail.com>
2023-05-12 01:28:57 +0200

    fixed Sidebar, which opens and closes the sidebar as desired

--- src/app/shared/offcanvas.directive.ts
+++ src/app/shared/offcanvas.directive.ts
@@ -1,51 +1,27 @@
-import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appOffcanvas]'
 })
-export class OffcanvasDirective {
+export class OffcanvasDirective {  
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
-  @HostBinding('class.show') isOpen = false;
+  @HostBinding('class.offcanvas-open') isOpen = false;
 
-  constructor(private elementRef: ElementRef) { }
-
-  @HostListener('click', ['$event.target'])
-  onClick(target: HTMLElement) {
+  @HostListener('click', ['$event']) onClick(event: MouseEvent) {
+    event.preventDefault();
     const offcanvas = document.querySelector(this.elementRef.nativeElement.dataset.bsTarget);
-    console.log(offcanvas);
-    if (offcanvas) {
-      if (target.classList.contains('navbar-toggler')) {
-        this.isOpen = !this.isOpen;
-        console.log('navbar-toggler' + this.isOpen);
-      } else if (target.classList.contains('btn-close')) {
-        this.isOpen = false;
-        console.log('btn-close' + this.isOpen);
-      } else {
-        this.isOpen = false;
-        console.log('no Targed found' + this.isOpen);
-      }
-      offcanvas.classList.toggle('show', this.isOpen);
-    }
+    this.isOpen = offcanvas.classList.toggle('show');
+    event.stopPropagation();
   }
 
 
-
-
-
-
-
-
-
-
-
-  @HostListener('document:click', ['$event.target'])
-  onDocumentClick(target: HTMLElement) {
+  @HostListener('window:click', ['$event']) onWindowClick(event: MouseEvent) {
     const offcanvas = document.querySelector(this.elementRef.nativeElement.dataset.bsTarget);
-    if (offcanvas && this.isOpen && !offcanvas.contains(target) && !target.classList.contains('navbar-toggler')) {
-      const backdrop = document.querySelector('.offcanvas-backdrop');
-      if (backdrop) {
-        backdrop.dispatchEvent(new Event('click'));
-      }
+    const isClickWithinOffcanvas = offcanvas.contains(event.target as Node);
+    if (!isClickWithinOffcanvas && this.isOpen) {
+      this.isOpen = false;
+      this.isOpen = offcanvas.classList.toggle('show');
     }
   }
 }
