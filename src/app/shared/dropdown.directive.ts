24d71886f00aed91f9d73c5d5092a5abaab7c946
zumbesa1 <salahudin.z@hotmail.com>
2023-05-11 16:27:46 +0200

    add directive to open Dropdown-menus

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@angular/core";
 
 @Directive({
     selector: '[appDropdown]'
@@ -7,7 +7,32 @@ import { Directive, HostBinding, HostListener } from "@angular/core";
 
 export class DropdownDirective {
     @HostBinding('class.show') isOpen = false;
+
+    constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
+  
     @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
-    }
+      const nativeElement = this.elementRef.nativeElement;
+      const dropdown = nativeElement.querySelector('.dropdown-menu');
+  
+      if (!this.isOpen) {
+        this.renderer.addClass(dropdown, 'show');
+        document.addEventListener('click', this.documentClickListener);
+      } else {
+        this.renderer.removeClass(dropdown, 'show');
+        document.removeEventListener('click', this.documentClickListener);
+      }
+      this.isOpen = !this.isOpen;
+    } 
+
+    private documentClickListener = (event: MouseEvent) => {
+        const targetElement = event.target as HTMLElement;
+        const dropdownElement = this.elementRef.nativeElement;
+        const isClickedInside = dropdownElement.contains(targetElement);
+
+        if (!isClickedInside) {
+            this.renderer.removeClass(dropdownElement.querySelector('.dropdown-menu'), 'show');
+            this.isOpen = false;
+            document.removeEventListener('click', this.documentClickListener);
+        }
+    }; 
 }
