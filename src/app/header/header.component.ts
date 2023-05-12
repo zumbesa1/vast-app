635366eacd46cb9965df596675d4b201bb22c703
zumbesa1 <salahudin.z@hotmail.com>
2023-05-12 04:42:18 +0200

    added icons and UI/UX improvements for smaller media

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit  {
+
+  constructor(private viewportScroller: ViewportScroller) {}
+  showIcons = false;
+
+  ngOnInit() {
+    this.checkMediaQuery();
+    window.addEventListener('resize', this.checkMediaQuery.bind(this));
+  }
+
+
+  checkMediaQuery() {
+    this.showIcons = window.innerWidth < 991;
+  }
+
 }
