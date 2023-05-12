caf404c0fbd3abc78a813aa6427ff44411f5a7a8
zumbesa1 <salahudin.z@hotmail.com>
2023-05-12 02:28:56 +0200

    fixed directive by using Observables instead of Hostlisteners

--- src/app/shared/offcanvas.directive.ts
+++ src/app/shared/offcanvas.directive.ts
@@ -1,27 +1,42 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, Renderer2, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 
 @Directive({
   selector: '[appOffcanvas]'
 })
-export class OffcanvasDirective {  
-  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
-
+export class OffcanvasDirective implements OnInit, OnDestroy {
   @HostBinding('class.offcanvas-open') isOpen = false;
 
-  @HostListener('click', ['$event']) onClick(event: MouseEvent) {
-    event.preventDefault();
-    const offcanvas = document.querySelector(this.elementRef.nativeElement.dataset.bsTarget);
-    this.isOpen = offcanvas.classList.toggle('show');
-    event.stopPropagation();
+  private clickSubscription!: Subscription;
+  private windowClickSubscription!: Subscription;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit() {
+    const nativeElement = this.elementRef.nativeElement; 
+    const offcanvas = document.querySelector(nativeElement.dataset.bsTarget);
+
+    this.clickSubscription = fromEvent<Event>(nativeElement, 'click')
+      .subscribe((event: Event) => {
+        event.preventDefault();
+        this.isOpen = offcanvas.classList.toggle('show');
+        console.log('click');
+        event.stopPropagation();
+      });
+
+    this.windowClickSubscription = fromEvent(window, 'click')
+      .subscribe((event: Event) => {
+        const isClickWithinOffcanvas = offcanvas.contains(event.target as Node);
+        if (!isClickWithinOffcanvas && this.isOpen) {
+          this.isOpen = false;
+          console.log('windowclick');
+          this.isOpen = offcanvas.classList.toggle('show');
+        }
+      });
   }
 
-
-  @HostListener('window:click', ['$event']) onWindowClick(event: MouseEvent) {
-    const offcanvas = document.querySelector(this.elementRef.nativeElement.dataset.bsTarget);
-    const isClickWithinOffcanvas = offcanvas.contains(event.target as Node);
-    if (!isClickWithinOffcanvas && this.isOpen) {
-      this.isOpen = false;
-      this.isOpen = offcanvas.classList.toggle('show');
-    }
+  ngOnDestroy() {
+    this.clickSubscription.unsubscribe();
+    this.windowClickSubscription.unsubscribe();
   }
 }
