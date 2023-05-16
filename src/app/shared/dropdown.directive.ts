bdc639eb0f20580e68f1c9558184019504f128f1
zumbesa1 <salahudin.z@hotmail.com>
2023-05-16 02:47:08 +0200

    refactored Hostlistener with Subsctiption RxJs

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,39 +1,52 @@
-import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@angular/core";
+import { Directive, ElementRef, HostBinding, OnDestroy, OnInit, Renderer2 } from "@angular/core";
+import { Subscription, fromEvent } from "rxjs";
 
 @Directive({
-    selector: '[appDropdown]'
+  selector: '[appDropdown]'
 })
 
 
-export class DropdownDirective {
-    @HostBinding('class.show') isOpen = false;
+export class DropdownDirective implements OnInit, OnDestroy{
+  @HostBinding('class.show') isOpen = false;
+  private clickSubscription!: Subscription;
+  private documentClickSubscription!: Subscription;
 
-    constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
-  
-    @HostListener('click') toggleOpen() {
-      const nativeElement = this.elementRef.nativeElement;
-      const dropdown = nativeElement.querySelector('.dropdown-menu');
-  
-      if (!this.isOpen) {
-        this.renderer.addClass(dropdown, 'show');
-        this.isOpen = true;
-        document.addEventListener('click', this.documentClickListener);
-      } else {
-        this.renderer.removeClass(dropdown, 'show');
-        this.isOpen = false;
-        document.removeEventListener('click', this.documentClickListener);
-      }
-    } 
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
-    private documentClickListener = (event: MouseEvent) => {
-        const targetElement = event.target as HTMLElement;
-        const dropdownElement = this.elementRef.nativeElement;
-        const isClickedInside = dropdownElement.contains(targetElement);
+  ngOnInit(): void {
+    const nativeElement = this.elementRef.nativeElement;
+    const dropdown = nativeElement.querySelector('.dropdown-menu');
 
-        if (!isClickedInside) {
-            this.renderer.removeClass(dropdownElement.querySelector('.dropdown-menu'), 'show');
-            this.isOpen = false;
-            document.removeEventListener('click', this.documentClickListener);
+    this.clickSubscription = fromEvent<Event>(nativeElement, 'click').
+      subscribe((event: Event) => {
+        console.log('clicksubscribtion');
+        event.preventDefault();
+        if (!this.isOpen) {
+          this.renderer.addClass(dropdown, 'show');
+          this.isOpen = true;
+          document.addEventListener('click', this.documentClickListener);
+        } else {
+          this.renderer.removeClass(dropdown, 'show');
+          this.isOpen = false;
+          document.removeEventListener('click', this.documentClickListener);
         }
-    }; 
+      });
+  }
+
+  ngOnDestroy() {
+    this.clickSubscription.unsubscribe();
+    this.documentClickSubscription.unsubscribe();
+  }
+
+  private documentClickListener = (event: MouseEvent) => {
+    const targetElement = event.target as HTMLElement;
+    const dropdownElement = this.elementRef.nativeElement;
+    const isClickedInside = dropdownElement.contains(targetElement);
+
+    if (!isClickedInside) {
+      this.renderer.removeClass(dropdownElement.querySelector('.dropdown-menu'), 'show');
+      this.isOpen = false;
+      document.removeEventListener('click', this.documentClickListener);
+    }
+  };
 }
